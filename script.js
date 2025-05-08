5f6e5bfc9e49160bfd0a0523dc9861483656c011
roberto <robertmoto510@gmail.com>
2025-05-08 14:49:25 -0300

    estrutura da fução for

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
 
 for (let i = 1; i <= 10; i++){
-  console.log(`passo: ${i}`)
+console.log(`passo: ${i}`)
 }
-
-
 
 for (let i = 2; i <= 20; i = i + 2){
   console.log(`passo: ${i}`)
 }
+
+for (let i = 1; i <= 10; i = i + 2){
+  console.log(`passo: ${i}`)
+}
+
+for (let i = 1; i <= 10; i++){
+  if(i >=10 %2 !== 0 ){
+    //ou if(i >=10 %2 === 1) para dar resultados impares.
+console.log(`passo: ${i}`)}
+}
+
+let numero = prompt("digite um numero");
+for(let i = 1; i <= 10; i++){
+  console.log(` o resultado ${numero * i}`) //para apenas resultado
+  // ou (`${numero} x ${i} = ${numero i* i}`) para aparcer todo calculo
+
+}
+
+
+
+
+
+
+
+
+
+
 
       

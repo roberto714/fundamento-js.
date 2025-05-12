d556d96418b3d4f0253bc4f0dacddb3b9388128c
roberto <robertmoto510@gmail.com>
2025-05-12 14:18:43 -0300

    funçao callback

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
-let senha = number(prompt("digite a senha:"));
-while(senha !== 1234){
-alert("acesso negado!");
-senha = number(prompt("senha invalida.tente novamente"));
-}
-alert("acesso permitido")
+//let senha = number(prompt("digite a senha:"));
+//while(senha !== 1234){
+//alert("acesso negado!");
+//senha = number(prompt("senha invalida.tente novamente"));
+//}
+//alert("acesso permitido")
 
-let numero = 10;
-while(numero >= 1){
-console.log(numero);
-numero--;
+//const { useCallback } = require("react")
+
+//let numero = 10;
+//while(numero >= 1){
+//console.log(numero);
+//numero--;
+//}
+//let palavra = prompt("digite uma palavra");
+//let contador = 1;
+//while(contador <= 5){
+//console.log(palavra);
+//contador++
+// }
+
+function verificarNumero(numero,callback){
+if(numero >= 10){
+    callback(`${"numero alto"}`)
+} else {
+    callback(`${"numero baixo"}`)
 }
-let palavra = prompt("digite uma palavra");
-let contador = 1;
-while(contador <= 5){
-console.log(palavra);
-contador++
- }    
+}
+function mensagem(texto){
+    console.log(texto)
+}
+
+verificarNumero(11,mensagem)

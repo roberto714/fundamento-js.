c6410dafd9d4df760cf3313a5e957282a38a9e3f
roberto <robertmoto510@gmail.com>
2025-05-13 13:35:55 -0300

    boas vindas

--- script.js
+++ script.js
@@ -1,33 +1,8 @@
-//let senha = number(prompt("digite a senha:"));
-//while(senha !== 1234){
-//alert("acesso negado!");
-//senha = number(prompt("senha invalida.tente novamente"));
-//}
-//alert("acesso permitido")
-
-//const { useCallback } = require("react")
-
-//let numero = 10;
-//while(numero >= 1){
-//console.log(numero);
-//numero--;
-//}
-//let palavra = prompt("digite uma palavra");
-//let contador = 1;
-//while(contador <= 5){
-//console.log(palavra);
-//contador++
-// }
-
-function verificarNumero(numero,callback){
-if(numero >= 10){
-    callback(`${"numero alto"}`)
-} else {
-    callback(`${"numero baixo"}`)
-}
-}
-function mensagem(texto){
-    console.log(texto)
+function boasvindas(nome,callback){
+    callback(nome);
 }
 
-verificarNumero(11,mensagem)
+function saudar(nome){
+    console.log(`olá,${nome}!`)
+}
+boasvindas("roberto",saudar);
